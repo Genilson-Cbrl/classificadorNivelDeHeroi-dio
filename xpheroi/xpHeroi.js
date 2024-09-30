bc08484c4bdd3fdc4762875d7e1f1ecab8f91503
Genilson-Cbrl <167100342+Genilson-Cbrl@users.noreply.github.com>
2024-09-30 19:41:37 -0300

    enviando para o repositorio

---
+++ xpheroi/xpHeroi.js
@@ -0,0 +1,22 @@
+const xpHeroi=101000
+const nomeHeroi="hulk"
+let nivel;
+
+if(xpHeroi<=1000){
+    nivel="Ferro"
+}else if(xpHeroi>1000&&xpHeroi<=2000){
+    nivel="Bronze"
+}else if(xpHeroi>2000&&xpHeroi<=5000){
+    nivel="Prata"
+}else if(xpHeroi>5000&&xpHeroi<=7000){
+    nivel="Ouro"
+}else if(xpHeroi>7000&&xpHeroi<=8000){
+    nivel="Platina"
+}else if(xpHeroi>8000&&xpHeroi<=9000){
+    nivel="Ascendente"
+}else if(xpHeroi>9000&&xpHeroi<=10000){
+    nivel="Imortal"
+}else if(xpHeroi>10000){
+    nivel="Radiante"
+}
+console.log(`O Heroi de nome ${nomeHeroi} esta no nivel de ${nivel}`)
